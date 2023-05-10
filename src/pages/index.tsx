ac777f133040508432490b28c7ec3787a51f5614
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-05-11 03:40:57 +0800

    fixing index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import { Sign } from "crypto";
 import { type NextPage } from "next";
 import Link from "next/link";
 import { useState } from "react";
+import LoaderModal from "~/components/LoaderModal";
 import Login from "~/components/Login";
 import Signup from "~/components/Signup";
 import useReverseAuth from "~/hooks/useReverseAuth";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const data = useReverseAuth()
+  const {isLoading, isError} = useReverseAuth()
   const [content, setContent] = useState<'login' | 'signup'>('login')
   
   const handleChangeContent = (content: 'login' | 'signup') => {
@@ -17,6 +18,8 @@ const Home: NextPage = () => {
   type changeContent = (content: 'login' | 'signup') => void
   
   const contentPage = content === 'login' ? <Login handleChangeContent={handleChangeContent as changeContent }  /> : <Signup handleChangeContent={handleChangeContent as changeContent} />
+  if(isLoading) return <LoaderModal />
+  if(isError) return <div>Something went wrong...</div>
   return (
       <main className="flex min-h-screen flex-col items-center justify-center">
         {contentPage}
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -151,7 +151,7 @@ const index = () => {
                             
                             <div className="flex flex-col">
                                 <div className="flex items-end">
-                                    <span className="text-2xl 2xl:text-3xl font-bold">217</span>
+                                    <span className="text-2xl 2xl:text-3xl font-bold">{quiz?.length}</span>
                                     <div className="flex items-center ml-2 mb-1">
                                         <svg className="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
                                     </div>
@@ -178,103 +178,10 @@ const index = () => {
                 </div>
 
                 <div className="mt-4">
-                    {/* <canvas id="verticalBarChart" style="display: block; box-sizing: border-box; height: 414px; width: 828px;" width="1656" height="828"></canvas> */}
                 </div>
             </div>
         </div>
     </div>
-    {/* <div className="bg-white rounded-lg shadow-xl p-8">
-        <div className="flex items-center justify-between">
-            <h4 className="text-xl text-gray-900 font-bold">Connections (532)</h4>
-            <a href="#" title="View All">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500 hover:text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-                </svg>
-            </a>
-        </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-8 mt-8">
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection1.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Diane Aguilar</p>
-                <p className="text-xs text-gray-500 text-center">UI/UX Design at Upwork</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection2.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Frances Mather</p>
-                <p className="text-xs text-gray-500 text-center">Software Engineer at Facebook</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection3.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Carlos Friedrich</p>
-                <p className="text-xs text-gray-500 text-center">Front-End Developer at Tailwind CSS</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection4.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Donna Serrano</p>
-                <p className="text-xs text-gray-500 text-center">System Engineer at Tesla</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection5.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Randall Tabron</p>
-                <p className="text-xs text-gray-500 text-center">Software Developer at Upwork</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection6.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">John McCleary</p>
-                <p className="text-xs text-gray-500 text-center">Software Engineer at Laravel</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection7.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Amanda Noble</p>
-                <p className="text-xs text-gray-500 text-center">Graphic Designer at Tailwind CSS</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection8.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Christine Drew</p>
-                <p className="text-xs text-gray-500 text-center">Senior Android Developer at Google</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection9.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Lucas Bell</p>
-                <p className="text-xs text-gray-500 text-center">Creative Writer at Upwork</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection10.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Debra Herring</p>
-                <p className="text-xs text-gray-500 text-center">Co-Founder at Alpine.js</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection11.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Benjamin Farrior</p>
-                <p className="text-xs text-gray-500 text-center">Software Engineer Lead at Microsoft</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection12.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Maria Heal</p>
-                <p className="text-xs text-gray-500 text-center">Linux System Administrator at Twitter</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection13.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Edward Ice</p>
-                <p className="text-xs text-gray-500 text-center">Customer Support at Instagram</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection14.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Jeffery Silver</p>
-                <p className="text-xs text-gray-500 text-center">Software Engineer at Twitter</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection15.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Jennifer Schultz</p>
-                <p className="text-xs text-gray-500 text-center">Project Manager at Google</p>
-            </a>
-            <a href="#" className="flex flex-col items-center justify-center text-gray-800 hover:text-blue-600" title="View Profile">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/connections/connection16.jpg" className="w-16 rounded-full"/>
-                <p className="text-center font-bold text-sm mt-1">Joseph Marlatt</p>
-                <p className="text-xs text-gray-500 text-center">Team Lead at Facebook</p>
-            </a>
-        </div>
-    </div> */}
 </div>
 }
 
