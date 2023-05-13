12fb21e555841c137d0943ea23ab29175c0c837f
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-05-13 13:16:19 +0800

    doc viewer final fix

--- src/components/ReviewerContent.tsx
+++ src/components/ReviewerContent.tsx
@@ -3,6 +3,7 @@ import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import { Reviewer } from "@prisma/client";
 import Timer from "./Timer";
 import styles from "./style.module.css";
+import { ex } from "@fullcalendar/core/internal-common";
 interface Props {
   data: Reviewer;
 }
@@ -17,32 +18,28 @@ const ReviewerContent = ({ data }: Props) => {
   ];
   const ext = data?.image_url?.split(".")[3];
   const fileName = data?.image_url?.split("/")[8]?.split(".")[0];
+
+  let content;
+
+  if(ext === '' || !ext) content = null;
+  if(ext === 'pdf') content = <iframe className="w-[60vw] h-[100vh] max-[900px]:flex-col max-[900px]:w-screen max-[900px]:h-[65vh]" src={`https://docs.google.com/viewer?embedded=true&url=${data?.image_url}`} ></iframe>
+  else if (ext === "pptx") content = <iframe className="w-[60vw] h-[100vh] max-[900px]:flex-col max-[900px]:w-screen max-[900px]:h-[65vh]" src={`https://view.officeapps.live.com/op/view.aspx?src=http%3A%2F%2Fres%2Ecloudinary%2Ecom%3A80%2Fiamprogrammer%2Fraw%2Fupload%2Fv1683728668%2Fonline-student-reviewer%2F${fileName}%2Epptx&wdSlideId=&wdModeSwitchTime=1683949969946`}></iframe>
+  else content = <DocViewer
+  documents={docs}
+  pluginRenderers={DocViewerRenderers}
+  config={{}}
+  className="h-[50vh] w-[50vw]"
+  style={{
+    width: "100%",
+    height: "auto",
+    minHeight: "70vh",
+    flex: 1,
+  }}
+  ref={viewerRef}
+/>
   return (
     <div className={styles.styleReview}>
-      {/* {data?.image_url && (
-        <DocViewer
-          documents={docs}
-          pluginRenderers={DocViewerRenderers}
-          config={{}}
-          className="h-[50vh] w-[50vw]"
-          style={{
-            width: "100%",
-            height: "auto",
-            minHeight: "70vh",
-            flex: 1,
-          }}
-          ref={viewerRef}
-        />
-      )} */}
-      {ext === "pdf" ? (
-        <iframe className="w-[60vw] h-[100vh] max-[900px]:flex-col max-[900px]:w-screen max-[900px]:h-[65vh]" src={`https://docs.google.com/viewer?embedded=true&url=${data?.image_url}`} ></iframe>
-      ) : (
-        <iframe
-          className="w-[60vw] h-[100vh] max-[900px]:flex-col max-[900px]:w-screen max-[900px]:h-[65vh]"
-          src={`https://view.officeapps.live.com/op/view.aspx?src=http%3A%2F%2Fres%2Ecloudinary%2Ecom%3A80%2Fiamprogrammer%2Fraw%2Fupload%2Fv1683728668%2Fonline-student-reviewer%2F${fileName}%2Epptx&wdSlideId=&wdModeSwitchTime=1683949969946`}
-        ></iframe>
-      )}
-
+      {content}
       <Timer data={data} />
     </div>
   );
