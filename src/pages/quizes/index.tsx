351897282c7e91b113d1971879d7027795c83aeb
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-06-26 02:08:51 +0800

    socket io

--- src/pages/quizes/index.tsx
+++ src/pages/quizes/index.tsx
@@ -16,7 +16,7 @@ const index = () => {
   }
   
 useEffect(() => {
-  const socket = io('https://study-with-me-atf3.vercel.app/api/root')
+  const socket = io('https://study-with-me-atf3.vercel.app/api')
 
   socket.emit('hello', {})
   socket.on('hello_back', () => {
